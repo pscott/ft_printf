60e136778cd2a7268b9acef9d1f20c442bbdd205
Piriou SCOTT <pscott@student.42.fr>
2018-11-29 17:10:04 +0100

    added main.c : working product

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pscott <pscott@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 11:34:09 by pscott            #+#    #+#             */
-/*   Updated: 2018/11/29 14:49:38 by pscott           ###   ########.fr       */
+/*   Updated: 2018/11/29 17:07:45 by pscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,31 +47,3 @@ int		ft_printf(const char *format, ...)
 	/* return value not good*/
 	return (1);
 }
-
-int	main(void)
-{
-	unsigned char c;
-	ULL i;
-	char *str;
-
-	c = 'A';
-	i = -12;
-	i = (int)i;
-
-	str = ":%+###005d:\n";
-	ft_printf(str, i, i + 3);
-	   printf(str, i , i + 3);
-	return (0);
-}
-
-/* plutot que de ft_putchar, il faut compter la taille de la string une fois. Donc nb extra char + max(width, value) pour chaque value.*/
-/* need to create struct, put all info in it, then (maybe) use pointer funcions to print the struct with specs, then rince and repeat !*/
-/*A partir de la struct, calculer la len (max width, len). 
- * Si +, - et value >= 0: len +=1, padding avec 'space'
- * Si +, -, value < 0: len, padding avec 'space'
- * Si +, value > 0, len += 1
- * Si +, value < 0: len.
- * Si -, padding gauche, peu importe
- *
- * Il faut gerer le re-alloc
- */
