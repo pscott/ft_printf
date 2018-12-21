095517b5dd33fab2d317d38af0fe1d19b54945ac
Piriou SCOTT <pscott@student.42.fr>
2018-12-21 18:41:48 +0100

    norminette and opti getters

--- srcs/getters.c
+++ srcs/getters.c
@@ -6,11 +6,38 @@
 /*   By: pscott <pscott@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 14:11:03 by pscott            #+#    #+#             */
-/*   Updated: 2018/12/20 20:50:06 by pscott           ###   ########.fr       */
+/*   Updated: 2018/12/21 18:41:27 by pscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	check_if_flags(t_arg *specs, char c, char **format)
+{
+	if (c == '-')
+		specs->left += 1;
+	else if (c == '+')
+		specs->plus += 1;
+	else if (c == '#')
+		specs->hash += 1;
+	else if (c == '0')
+		specs->fill = '0';
+	else if (c == ' ')
+	{
+		set_extra(c, specs);
+		if (specs->fill != '0')
+			specs->fill = ' ';
+	}
+	else if (ft_isdigit(c))
+	{
+		if ((specs->width_len = ft_atoi_move(format)))
+		{
+			specs->width = 1;
+			increm_format(format, -1);
+		}
+	}
+	increm_format(format, 1);
+}
 
 int		get_flags(char **format, t_arg *specs)
 {
@@ -30,38 +57,10 @@ int		get_flags(char **format, t_arg *specs)
 			(*format)--;
 		}
 		if (is_type(format, specs))
-		{
-			specs->type = **format;
-			return (1);
-		}
+			return (specs->type = **format);
 		if (is_spec_upper(**format))
-		{
-			specs->type = '%';
-			return (1);
-		}
-		if (c == '-')
-			specs->left += 1;
-		else if (c == '+')
-			specs->plus += 1;
-		else if (c == '#')
-			specs->hash += 1;
-		else if (c == '0')
-			specs->fill = '0';
-		else if (c == ' ')
-		{
-			set_extra(c, specs);
-			if (specs->fill != '0')
-				specs->fill = ' ';
-		}
-		else if (ft_isdigit(c))
-		{
-			if ((specs->width_len = ft_atoi_move(format)))
-			{
-				specs->width = 1;
-				increm_format(format, -1);
-			}
-		}
-		increm_format(format, 1);
+			return (specs->type = '%');
+		check_if_flags(specs, c, format);
 	}
 	return (0);
 }
@@ -71,7 +70,7 @@ void	get_lh(char **format, t_arg *specs)
 	int i;
 
 	i = 0;
-	while ((*format)[i] == 'h' || (*format)[i]  == 'l')
+	while ((*format)[i] == 'h' || (*format)[i] == 'l')
 	{
 		if ((*format)[i] == 'h')
 			specs->h++;
